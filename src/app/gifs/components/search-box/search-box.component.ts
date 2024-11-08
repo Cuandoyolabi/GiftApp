6f18d71e58da0f68431da85a64a3d211970782e2
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-08 14:30:56 -0600

    Se modifican los componentes y se utiliza un decorador ViewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-box',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './search-box.component.css'
 })
 export class SearchBoxComponent {
+
+  @ViewChild("txtTagInput")
+  public tagInput!: ElementRef <HTMLInputElement>;
+
+  searchTag(){
+
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log({ newTag })
+  }
 
 }
