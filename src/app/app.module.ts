cbed4378f537d3c27a761c34bc3d484616ecf283
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-08 12:21:54 -0600

    Se agregan modulos y componentes nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//Modulos
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+
+//Componentes
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
