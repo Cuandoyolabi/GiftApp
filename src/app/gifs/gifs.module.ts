75d829fdd6cf07b6da5fb7088b712066f7559805
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-08 14:09:26 -0600

    Se creo un nuevo componente para el espacio de los gifts

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Componentes
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { ListComponent } from './components/list/list.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    ListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HomePageComponent,
-
   ]
 })
 export class GifsModule { }
