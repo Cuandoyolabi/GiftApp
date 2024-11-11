82b8c13724f9569c2c5aa678b21c174aef6dc05b
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-11 13:11:55 -0600

    Se continua con el proyecto y ahora se enfoca en mostrar los gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = "MiTGP50c6uJWhfwGgzoPRRSocsWsFH6a";
+
 
   constructor() { }
 
@@ -11,17 +13,26 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  public searchTag( tag : string): void {
+  private organizeHistory( tag : string ){
+    tag = tag.toLowerCase();
 
-    tag = tag.trim().toLocaleLowerCase();
-
-    if(tag && !this._tagsHistory.includes(tag)){
-      this._tagsHistory.unshift(tag);
+    if(this._tagsHistory.includes( tag )){
+      this._tagsHistory = this._tagsHistory.filter( (oldTag ) => oldTag !== tag)
     }
 
-    if(this._tagsHistory.length > 10){
-      this._tagsHistory.pop();
-    }
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0,10);
   }
 
+  public searchTag( tag : string): void {
+
+    if( tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=MiTGP50c6uJWhfwGgzoPRRSocsWsFH6a&q=Valorant&Limit=10")
+      .then( resp => resp.json())
+      .then( data => console.log(data));
+    }
 }
+
+
