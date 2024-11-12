6a169ac4a75855b9e705f625911a86a3ea2d68bc
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-12 12:27:16 -0600

    Se crea nuevo componente que se responsabilisa de renderizar

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -5,13 +5,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { ListComponent } from './components/list/list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    ListComponent
+    ListComponent,
+    GifsCardComponent
   ],
   imports: [
     CommonModule
